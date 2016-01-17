7655bf878434c2344ee146021fa112f0b7654283
Jan Wirth <jottenwe@gmail.com>
2016-01-17 20:34:54 +0100

    Add mongodb

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 import Scraper from '../amazon-review-scraper/'
+import {MongoClient} from 'mongodb'
+import assert from 'assert'
 
-console.log(Scraper)
+var scraper = new Scraper()
+
+var url = 'mongodb://localhost:27017/reviews'
+
+MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db.close();
+});
