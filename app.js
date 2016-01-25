1c71ab2881e51c11fa87ed395ed801c0e8f3ce6b
Jan Wirth <jottenwe@gmail.com>
2016-01-26 00:23:04 +0100

    Create analyze function stub

--- app.js
+++ app.js
@@ -11,30 +11,56 @@ var products = [
     'http://www.amazon.com/Ozeri-Digital-Multifunction-Kitchen-Elegant/dp/B004164SRA/ref=zg_bs_kitchen_2',
     'http://www.amazon.com/Blue-Microphones-Yeti-USB-Microphone/dp/B002VA464S/ref=zg_bs_musical-instruments_11'
 ]
-
-for (var url of products) {
-    scraper.scrapeProduct(url).then(function (productData) {
-        MongoClient.connect(dburl, function (err, db) {
-            assert.equal(null, err);
-            var collection = db.collection('products');
-            productData._id = productData.id;
-            delete productData.id;
-            collection.insert(productData);
-            console.log('inserted product')
-        });
-    });
-    scraper.scrapeProductReviews(url).then(function (reviews) {
-        MongoClient.connect(dburl, function (err, db) {
-            assert.equal(null, err);
-            var collection = db.collection('reviews');
-            for (var review of reviews){
-                review._id = review.id;
-                delete review.id;
-                collection.insert(review);
-                console.log('inserted review')
-            }
+var scrapeProducts = function (products) {
+    for (var url of products) {
+        scraper.scrapeProduct(url).then(function (productData) {
+            MongoClient.connect(dburl, function (err, db) {
+                assert.equal(null, err);
+                var collection = db.collection('products');
+                productData._id = productData.id;
+                delete productData.id;
+                collection.insert(productData);
+                console.log('inserted product')
             });
+        });
+        scraper.scrapeProductReviews(url).then(function (reviews) {
+            MongoClient.connect(dburl, function (err, db) {
+                assert.equal(null, err);
+                var collection = db.collection('reviews');
+                for (var review of reviews){
+                    review._id = review.id;
+                    delete review.id;
+                    collection.insert(review);
+                    console.log('inserted review')
+                }
+                });
+        });
+    }
+};
+
+// scrapeProducts(products);
+
+
+
+var analyzeReviews = function() {
+    MongoClient.connect(dburl, function (err, db) {
+        assert.equal(null, err);
+        var rawdataCollection = db.collection('reviews');
+        var productCollection = db.collection('products');
+        var targetCollection = db.collection('analyzedReviews');
+        rawdataCollection.find().toArray(function(err, res) {
+            for (var rawReview of res){
+                var processedReview = rawReview;
+                processedReview.votes.quota = rawReview.votes.helpful / rawReview.votes.total
+                processedReview.languageMetaData = {}
+                processedReview.languageMetaData.length = processedReview.text.length
+                delete processedReview.text;
+                console.log(processedReview);
+                // targetCollection.insert(review);
+            }
+            db.close()
+        });
     });
 }
 
-scraper.scrapeProductReviews
+analyzeReviews();
